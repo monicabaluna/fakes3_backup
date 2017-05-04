f712b74afd7da33ac684d81c2a16258ba128032f
Monica Baluna <monicamariabaluna@gmail.com>
2017-05-04 18:23:16 +0300

    archive_db: add command to check restored database integrity

--- fetcher/archive_db.sh
+++ fetcher/archive_db.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+/opt/fetcher/fill_database.js $1
+
 ARCHIVE=/opt/fetcher/dump
 
-rm $ARCHIVE
 mongodump --host mongo --port 27017 --db $1 --gzip --archive=$ARCHIVE
+
+/opt/fetcher/empty_database.js $1
 
 export AWS_ACCESS_KEY_ID=1234
 export AWS_SECRET_ACCESS_KEY=1234
@@ -11,3 +14,9 @@ export AWS_DEFAULT_REGION=us-west-2
 
 aws --endpoint-url http://s3-service:4567 s3 mb s3://mongo_backup
 aws --endpoint-url http://s3-service:4567 s3 cp $ARCHIVE s3://mongo_backup/dump
+
+sleep 10
+
+/opt/fetcher/check_database.js $1
+
+rm $ARCHIVE
